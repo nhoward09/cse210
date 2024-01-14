a05aecbb581e96581aaddd007b459079f25a0b12
nhoward09 <how19012@byui.edu>
2024-01-13 17:57:16 -0700

    did homework

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // to use print, address the console, then writeline()
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+
+
+        System.Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
+
+
+
+
+
+// //primitive types
+
+// int i;
+// long l;
+// float f;
+// double d;
+// char c;
+// string s;
+// byte b;
+// bool b2;
+
+
+// //variable
+// int myCount = 4;
+// string aName = "bob";
+
+
+// //var
+// var anotherCount = 4; //takes on the property of the first item in the variable
+//                       //in general, just use var
+
+
+// //Printing Variables
+// Console.Write("Whats your age?");
+
+// //ReadLine
+// var ageString = Console.ReadLine();
+// System.Console.WriteLine($"My age is {ageString}.");
+
+// //Converting Variables
+// int age = int.Parse(ageString);
+
+// //conditionals
+// if (age < 18)
+//     System.Console.WriteLine("You're a minor");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the number you will be testing? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter;
+
+        if (percent >= 90)
+            letter = "A";
+
+        else if (percent >= 80)
+            letter = "B";
+
+        else if (percent >= 70)
+            letter = "C";
+
+        else if (percent >= 60)
+            letter = "D";
+
+        else
+            letter = "F";
+
+        System.Console.WriteLine($"Your grade is a {letter}");
+
+        if (percent < 70)
+            System.Console.WriteLine("yOu fAiLeD!");
+
+        else
+            System.Console.WriteLine("Congrats you smarty pants! You passed!");
+
+
+
     }
 }
